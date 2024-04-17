5544e01152a305b02403e3ad7099344b4b5de84c
ArenaCup5273 <90076788+ArenaCup5273@users.noreply.github.com>
2024-04-16 18:21:53 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,18 @@
-//Mostrar y ocultar contraseña
-var togglee = false;
-
-function mostrar (id) {
-    const fieldContraseña = document.getElementById(id);
-    togglee = !togglee;
-    if (togglee){
-        fieldContraseña.setAttribute("type","text");
-    } else {
-        fieldContraseña.setAttribute("type", "password");
-    }
-}
-
-//Cambiar de tema
-var toggleModo = false;
-
-const btnOscuro = document.querySelector(".btnTemaOscuro");
-const parrafo = document.getElementById("hola");
-const modoOscuro = () => {
-    toggleModo = !toggleModo;
-    if (toggleModo){
-        //Modo oscuro
-    } else {
-        //Modo claro
-    }
-}
-
 //Deslizar a destino
-function deslizar(idDestino) {
-    var destino = document.getElementById(idDestino);
-    destino.scrollIntoView({ behavior: 'smooth' });
+function deslizar(idDestino) { //Se declara la función para deslizar la pantalla, y se pone como parámetro un string llamado idDestino
+    var destino = document.getElementById(idDestino); //Obtener el elemento destino con el id que se obtiene del parámetro
+    destino.scrollIntoView({ behavior: 'smooth' }); //Deslizar con transición "smooth" al elemento de destino
 }
 
 // Deslizar al inicio
-function regresarInicio() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+function regresarInicio() { //Se declara la función para deslizar al inicio de la página
+    window.scrollTo({ top: 0, behavior: 'smooth' }); //Se utiliza el método scrollTo() para deslizar al inicio de la página (top:0), con una transición smooth
 }
 
-// Mostrar el botón cuando el usuario haya bajado cierta cantidad de píxeles
-window.onscroll = function() {
-    var botonRegresar = document.querySelector('.boton-regresar');
-    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+// Mostrar el botón
+window.onscroll = function() { //Se declara la función para el botón de regresar (se utiliza el window.onscroll para que la función se ejecute al scrollear en la página)
+    var botonRegresar = document.querySelector('.boton-regresar'); //Se obtiene el botón con la clase "boton-regresar"
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) { //Se comprueba a
         botonRegresar.style.display = "block";
     } else {
         botonRegresar.style.display = "none";
